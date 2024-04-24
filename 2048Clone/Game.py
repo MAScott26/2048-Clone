6858bc453b4175cfbc759da772def8f1b44429f0
MAScott26 <michael.scott@bsu.edu>
2024-04-23 22:48:18 -0400

    dear lord this sucks

--- 2048Clone/Game.py
+++ 2048Clone/Game.py
@@ -54,67 +54,126 @@ class Game(simpleGE.Scene):
     def __init__(self):
         super().__init__()
         self.setImage("Background.png")
-        self.square = Square(self)
         self.grid = Grid(self)
-        self.sprites = [self.grid, self.square]
+        self.tiles = []
+        for i in range(12):
+            self.tiles.append(Square(self))
+            
+            
+            
+        """self.tiles.append(Square(self))
+        self.square.currState = 1
+        self.square.imgUpdate()
+        self.tiles.append(self.square)"""
         
+        self.sprites = [self.grid, self.tiles]
+        
+    def process(self):
+        for i in self.tiles:
+            for j in self.tiles:
+                if i != j:
+                    if i.currState == j.currState:
+                        if i.collidesWith(j):
+                            print(i.currState)
+                            j.hide()
+                            i.currState += 1
+                            print(i.currState)
+                            i.imgUpdate()
+                            j.x = random.randrange(170, 471, 100)
+                            j.y = random.randrange(90, 391, 100)
+                            j.show()
+                        
+                        
 class Square(simpleGE.Sprite):
     def __init__(self, scene):
         super().__init__(scene)
-        self.setImage("imgOne.png")
+        self.currState = 0
+        self.squareState = [
+            "imgOne.png",
+            "imgTwo.png",
+            "imgFour.png",
+            "imgEight.png",
+            "imgSixT.png",
+            "imgThirT.png",
+            "img64.png",
+            "img128.png",
+            "img256.png",
+            "img512.png",
+            "img1024.png",
+            "img2048.png"
+            ]
+        self.setImage(self.squareState[self.currState])
         self.setSize(95, 95)
         self.movespeed = 100
-        self.x = 170
-        self.y = 90
+        self.xValues = [170, 270, 370, 470]
+        self.yValues = [90, 190, 290, 390]
+        self.x = random.randrange(170, 471, 100)
+        self.y = random.randrange(90, 391, 100)
         self.keyDown = False
-        self.squareState = {
-            "ref" : ["image", "prev", "Next"],
-            1 : ["imgOne.png", 1, 2],
-            2 : ["imgTwo.png", 2, 4],
-            4 : ["image", 4, 8],
-            8 : ["image", 8, 16],
-            16 : ["image", 16, 32],
-            32 : ["image", 32, 64],
-            64 : ["image", 64, 128],
-            128 : ["image", 128, 256],
-            256 : ["image", 256, 512],
-            512 : ["image", 512, 1024],
-            1024 : ["image", 1024, 2048],
-            2048 : ["image", 2048, 4096 ]
-            }
+        
+    def imgUpdate(self):
+        self.setImage(self.squareState[self.currState])
         
     def process(self):
         if self.isKeyPressed(pygame.K_LEFT):
             if self.x != 170:
-                
                 if self.keyDown == False:
-                    self.x -= self.movespeed
+                    move = True
+                    for i in range(3):
+                        for tile in self.scene.tiles:
+                            if tile.x + 100 == self.x and tile.y == self.y and tile.currState != self.currState:
+                                move = False
+                        if move:
+                            if self.x != 170:
+                                self.x -= self.movespeed
                     self.keyDown = True
                 else:
                     self.keyDown = True
         elif self.isKeyPressed(pygame.K_RIGHT):
             if self.x != 470:
                 if self.keyDown == False:
-                    self.x += self.movespeed
+                    move = True
+                    for i in range(3):
+                        for tile in self.scene.tiles:
+                            if tile.x - 100 == self.x and tile.y == self.y and tile.currState != self.currState:
+                                move = False
+                        if move:
+                            if self.x != 470:
+                                self.x += self.movespeed
                     self.keyDown = True
                 else:
                     self.keyDown = True
         elif self.isKeyPressed(pygame.K_UP):
             if self.y != 90:
                 if self.keyDown == False:
-                    self.y -= self.movespeed 
+                    move = True
+                    for i in range(3):
+                        for tile in reversed(self.scene.tiles):
+                            if tile.y + 100 == self.y and tile.x == self.x and tile.currState != self.currState:
+                                move = False
+                        if move:
+                            if self.y != 90:
+                                self.y -= self.movespeed 
                     self.keyDown = True
                 else:
                     self.keyDown = True
         elif self.isKeyPressed(pygame.K_DOWN):
             if self.y != 390:
                 if self.keyDown == False:
-                    self.y += self.movespeed
+                    move = True
+                    for i in range(3):
+                        for tile in self.scene.tiles:
+                            if tile.y - 100 == self.y and tile.x == self.x and tile.currState != self.currState:
+                                move = False
+                        if move:
+                            if self.y != 390:
+                                self.y += self.movespeed
                     self.keyDown = True
                 else:
-                        self.keyDown = True
+                    self.keyDown = True
         else:
             self.keyDown = False
+            
             
             
 def main():
@@ -127,10 +186,7 @@ def main():
         if startMenu.response == "Play":
             game = Game()
             game.start()
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    keepGoing = False
-            score = game.prevScore
+            keepGoing = False
         else:
             keepGoing = False
             
